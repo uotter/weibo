cace13a8dc40ee50feba3e979aaeba241347ee30
uotter <wangmengzju@gmail.com>
2015-09-18 17:12:08 +0800

    更新了部分对新数据进行分析的代码

--- data_ana.py
+++ data_ana.py
@@ -1,11 +1,6 @@
 import io
+import util
 __author__ = 'M'
-
-# f_train = open(".//data//weibo_train_data//weibo_train_data_new.txt")
-# f_test = open(".//data//weibo_predict_data//weibo_predict_data_new.txt")
-#
-# train_lines = f_train.readlines()
-# test_lines = f_test.readlines()
 
 
 def caculate_userids_proportion(train_lines, test_lines):
@@ -24,4 +19,39 @@ def caculate_userids_proportion(train_lines, test_lines):
         if userid in userids:
             count += 1
 
-    print count, total_test_num, (float(count)/float(total_test_num)), len(userids), len(train_lines)
+    print count, total_test_num, (float(count) / float(total_test_num)), len(userids), len(train_lines)
+
+
+def get_target_distribution(train_lines):
+    dic_f = {}
+    dic_c = {}
+    dic_l = {}
+    total_num = len(train_lines)
+    for line in train_lines:
+        single_line = line.decode("utf-8").split('\t')
+        f_num = single_line[3]
+        c_num = single_line[4]
+        l_num = single_line[5]
+        if dic_f.has_key(f_num):
+            dic_f[f_num] += 1
+        else:
+            dic_f[f_num] = 1
+        if dic_c.has_key(c_num):
+            dic_c[c_num] += 1
+        else:
+            dic_c[c_num] = 1
+        if dic_l.has_key(l_num):
+            dic_l[l_num] += 1
+        else:
+            dic_l[l_num] = 1
+    f_key_list = util.sort_by_value_dec(dic_f)
+    for key in f_key_list:
+        print str(key) + ': ' + str(dic_f[key]) + ' with ' + str(float(dic_f[key]) / float(total_num))
+
+
+f_train = open(".//data//weibo_train_data//weibo_train_data_new.txt")
+f_test = open(".//data//weibo_predict_data//weibo_predict_data_new.txt")
+
+train_lines = f_train.readlines()
+test_lines = f_test.readlines()
+get_target_distribution(train_lines)
